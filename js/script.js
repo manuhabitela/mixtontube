668cc924f717d9694cf32ddaf665cb0851f5d61b
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-01-16 00:55:48 +0100

    on a maintenant carrément un CLAVIER

--- js/script.js
+++ js/script.js
@@ -2,33 +2,20 @@
 	var soundSelector = '[data-son]',
 		$container = $('#container'),
 		$buttons = $(soundSelector, $container),
-		enhancedButtons = [];
+		enhancedButtons = [],
+		keyboard = "azertyuiopqsdfghjklmwxcvbn,;:!";
 	if (Modernizr.audio) {
-		$buttons.each(function(n, item) {
-			enhancedButtons.push('<div class="btn-group">');
-			enhancedButtons.push(item.outerHTML);
-			enhancedButtons.push('<a href="#" class="icon-repeat" data-play="loop" data-son="' + item.getAttribute('data-son') + '"></a>');
-			enhancedButtons.push('</div>');
-		});
-
-		$buttons.remove();
-		$container.append(enhancedButtons.join(''));
-
-		$container.on('click', soundSelector + '[data-play="once"]', function(e) {
-			var $this = $(this),
-				audio = document.createElement('audio');
-			audio.src = $this.attr('data-son');
+		var playOnce = function($el) {
+			var audio = document.createElement('audio');
+			audio.src = $el.attr('data-son');
 			audio.play();
-			e.preventDefault();
-		});
-
-		currentlyLooping = {};
-		$container.on('click', soundSelector + '[data-play="loop"]', function(e) {
-			var $this = $(this),
-				son = $this.attr('data-son');
+		};
+		var currentlyLooping = {};
+		var playInLoop = function($el) {
+			var son = $el.attr('data-son');
 
 			if (currentlyLooping[son] !== undefined) {
-				$this.removeClass('active');
+				$('[data-play="loop"][data-son="' + son + '"]').removeClass('active');
 				currentlyLooping[son].pause();
 				delete currentlyLooping[son];
 			} else {
@@ -37,9 +24,44 @@
 				audio.loop = true;
 				audio.play();
 				currentlyLooping[son] = audio;
-				$this.addClass('active');
+				$('[data-play="loop"][data-son="' + son + '"]').addClass('active');
 			}
+		};
 
+		$buttons.each(function(n, item) {
+			enhancedButtons.push('<div class="sound-container">');
+			enhancedButtons.push('<div class="btn-group">');
+			enhancedButtons.push(item.outerHTML);
+			enhancedButtons.push('<a href="#" class="repeat" data-play="loop" data-son="' + item.getAttribute('data-son') + '"></a>');
+			enhancedButtons.push('</div>');
+			enhancedButtons.push('<input class="key" disabled size=1 type=text maxlength=1 value="' + keyboard.charAt(n) + '">');
+			enhancedButtons.push('</div>');
+
+			var itemSelector = '[data-son="' + item.getAttribute('data-son') + '"]';
+			Mousetrap.bind(keyboard.charAt(n), function(e) {
+				playOnce($(itemSelector + '[data-play="once"]'));
+				$(itemSelector + '[data-play="once"]').addClass('active');
+			}, 'keydown');
+			Mousetrap.bind(keyboard.charAt(n), function(e) {
+				$(itemSelector + '[data-play="once"]').removeClass('active');
+			}, 'keyup');
+
+			Mousetrap.bind('shift+' + keyboard.charAt(n), function(e) {
+				playInLoop($(itemSelector + '[data-play="loop"]'));
+				e.preventDefault();
+			});
+		});
+
+		$buttons.remove();
+		$container.append(enhancedButtons.join(''));
+
+		$container.on('click', soundSelector + '[data-play="once"]', function(e) {
+			playOnce($(this));
+			e.preventDefault();
+		});
+		
+		$container.on('click', soundSelector + '[data-play="loop"]', function(e) {
+			playInLoop($(this));
 			e.preventDefault();
 		});
 	} else {
