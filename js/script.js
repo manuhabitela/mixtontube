47b9d616dc26c2a0a8dee0ac25f87fbaa1551f35
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-01-15 18:21:10 +0100

    Ajout d'un bouton pour lire en boucle un onsse yo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,49 @@
 (function() {
+	var soundSelector = '[data-son]',
+		$container = $('#container'),
+		$buttons = $(soundSelector, $container),
+		enhancedButtons = [];
 	if (Modernizr.audio) {
-		$('.son[data-file]').on('click', function(e) {
-			var audio = document.createElement('audio');
-			audio.src = $(this).attr('data-file');
+		$buttons.each(function(n, item) {
+			enhancedButtons.push('<div class="btn-group">');
+			enhancedButtons.push(item.outerHTML);
+			enhancedButtons.push('<a href="#" class="icon-repeat" data-play="loop" data-son="' + item.getAttribute('data-son') + '"></a>');
+			enhancedButtons.push('</div>');
+		});
+
+		$buttons.remove();
+		$container.append(enhancedButtons.join(''));
+
+		$container.on('click', soundSelector + '[data-play="once"]', function(e) {
+			var $this = $(this),
+				audio = document.createElement('audio');
+			audio.src = $this.attr('data-son');
 			audio.play();
+			e.preventDefault();
+		});
+
+		currentlyLooping = {};
+		$container.on('click', soundSelector + '[data-play="loop"]', function(e) {
+			var $this = $(this),
+				son = $this.attr('data-son');
+
+			if (currentlyLooping[son] !== undefined) {
+				$this.removeClass('active');
+				currentlyLooping[son].pause();
+				delete currentlyLooping[son];
+			} else {
+				var audio = document.createElement('audio');
+				audio.src = son;
+				audio.loop = true;
+				audio.play();
+				currentlyLooping[son] = audio;
+				$this.addClass('active');
+			}
+
+			e.preventDefault();
 		});
 	} else {
-		$('.son[data-file]').addClass('hidden');
-		$('body').append("<p>A MARCHE PÔ</p>");
+		$buttons.addClass('hidden');
+		$container.append("<p>A MARCHE PÔ</p>");
 	}
 })();
