1687e3ce202b21c2ba7d1d1d3f3d30b4f790c888
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-09-11 12:49:45 +0200

    bon, on utilise une checkbox pour les lectures en boucle

--- js/tontube.js
+++ js/tontube.js
@@ -27,6 +27,41 @@
 
 
 
+	var AudioUtils = {
+		element: function(file) {
+			var audio = document.createElement('audio');
+			var ext = ['mp3', 'ogg'];
+			for (var i = ext.length - 1; i >= 0; i--) {
+				var source = document.createElement('source');
+				source.src = file + '.' + ext[i];
+				audio.appendChild(source);
+			}
+			return audio;
+		},
+
+		loop: function(element) {
+			element.loop = true;
+			$(element).on("ended", function() {
+				this.currentTime = 0;
+				this.play();
+			});
+			element.play();
+		},
+
+		batchPlay: function(elements, position) {
+			if (typeof position === "undefined") position = 0;
+			currentElement = elements[pos];
+			if (currentElement) {
+				$(currentElement).on("ended", function() {
+					playNext(elements, pos+1);
+				});
+				currentElement.play();
+			}
+		}
+	};
+
+
+
 	var App = function(data, el, opts) {
 		if (!Modernizr.audio) {
 			$(el).replaceWith("<p>A MARCHE PÔ</p><p>Tu as surement un navigateur un peu vieux, essaie donc avec un Chrome ou Firefox.</p>");
@@ -89,20 +124,25 @@
 
 	Item.prototype = {
 		play: function(file) {
-			this._getAudioElement(file).play();
+			AudioUtils.element(file).play();
 		},
 
 		toggle: function(file, el) {
 			if (this.looping[file]) {
 				this.looping[file].pause();
 				this.looping[file] = false;
-				if (el) el.removeClass('looping');
+				if (el) {
+					el.removeClass('looping');
+					el.find('.item__sound-checkbox').prop('checked', '');
+				}
 			} else {
-				var audio = this._getAudioElement(file);
-				audio.loop = true;
-				audio.play();
+				var audio = AudioUtils.element(file);
+				AudioUtils.loop(audio);
 				this.looping[file] = audio;
-				if (el) el.addClass('looping');
+				if (el) {
+					el.addClass('looping');
+					el.find('.item__sound-checkbox').prop('checked', 'checked');
+				}
 			}
 		},
 
@@ -135,18 +175,19 @@
 		},
 
 		initClickListeners: function(e) {
-			this.$el.find('.item__sound-button').longpress(
-				_.bind(function(e) {
-					this.toggle( this._getAudioFile(e), $(e.currentTarget).closest('.item__sound') );
-				}, this),
-				_.bind(function(e) {
-					var file = this._getAudioFile(e);
-					if (this.looping[file])
-						this.toggle(file, $(e.currentTarget).closest('.item__sound'));
-					else
-						this.play(file);
-				}, this)
-			);
+			this.$el.on('click', '.item__sound-button', _.bind(function(e) {
+				var file = this._getAudioFile(e);
+				if (this.looping[file])
+					this.toggle(file, $(e.currentTarget).closest('.item__sound'));
+				else
+					this.play(file);
+
+				e.preventDefault();
+			}, this));
+
+			this.$el.on('click', '.item__sound-checkbox', _.bind(function(e) {
+				this.toggle( this._getAudioFile(e), $(e.currentTarget).closest('.item__sound') );
+			}, this));
 
 			this.$el.on('click', '.item__video-link', _.bind(function(e) {
 				$link = $(e.currentTarget);
@@ -164,7 +205,6 @@
 		initKeyListeners: function() {
 			var that = this;
 			_(this.data.sounds).each(function(sound) {
-				console.log(sound);
 				var $el = $('.item__sound[data-file="' + sound.file + '"]', that.$el);
 
 				Mousetrap.bind(sound.shortcut, function(e) {
@@ -180,17 +220,6 @@
 					that.toggle(sound.file, $el);
 				});
 			}, this);
-		},
-
-		_getAudioElement: function(file) {
-			var audio = document.createElement('audio');
-			var ext = ['mp3', 'ogg'];
-			for (var i = ext.length - 1; i >= 0; i--) {
-				var source = document.createElement('source');
-				source.src = file + '.' + ext[i];
-				audio.appendChild(source);
-			}
-			return audio;
 		},
 
 		_getAudioFile: function(event) {
